e4c2ab66b95e99ac62b38ff916d920cb0318f931
Albert Hidalgo <ahidalgo@syneteksolutions.com>
2020-05-21 11:41:14 -0400

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+## Simple express server for run statics sites
+
+```bash
+$ npm install
+```
+
+Start server
+
+```bash
+$ npm run server
+```
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/ping', function (req, res) {
+    return res.send('pong');
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT);
+
+console.log(`Server running in http://localhost:${process.env.PORT}`)
